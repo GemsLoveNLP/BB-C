8e70eed74a72ea8a5377ef0937c9f715595542dc
GemsLoveNLP <6438169421@student.chula.ac.th>
2023-04-08 21:51:40 +0700

    generator actually works now

--- generator.py
+++ generator.py
@@ -1,11 +1,5 @@
-import numpy as np
-import random as rd
-import math
-
-# ? Note: key is in top left, one key piece in block
-
 orientation_list = [ [[1,0],[0,1]], [[0,1],[-1,0]], [[-1,0],[0,-1]], [[0,-1],[1,0]] ] 
-shift_dict = { "L": [[0,0], [1,0], [1,-1], [0,-1]], "R": [[0,0], [2,0], [1,-1], [0,-1]]}
+shift_dict = { "L": [[0,0], [0,1], [1,1], [1,0]], "D": [[0,0], [2,0], [1,-1], [0,-1]]}
 
 def transform_hole(hole, orientation, piece):    
     matrix = np.array(orientation_list[orientation])
@@ -31,23 +25,30 @@ def draw(l):
     return s
 
 class block:
-    def __init__(self, piece, height, width, tall, holes=[], orientation=0):
+    def __init__(self, piece, height, width, tall, orientation, holes=[]):
         self.piece = piece
         self.height = height
         self.width = width
         self.tall = tall
-        self.holes = holes
-        self.orientation = orientation
+        self.holes = holes #list of holes, each hole is a list of [y,x]
+        self.orientation = orientation #num of possible oreintation
 
     def area(self):
         return self.height*self.width
     
 def setup():
-    global L
-    L = block("L",2,2,2,[[1,0]],0)
+    # pieces
+    global L, T, I, F
+    L = block("L",2,2,2,4,[[1,0]])
+    T = block("T",2,3,2,4,[[0,1],[2,1]])
+    I = block("I",2,1,2,2,[])
+    F = block("F",2,2,2,1,[])
+    #  constants
+    global threshold
+    threshold = 100
 
 class slot:
-    def __init__(self, piece='d'):
+    def __init__(self, piece=' '):
         self.piece = piece  
 
     def change_piece(self, new_piece):
@@ -68,7 +69,8 @@ def transform(array):
         l.append(temp)
     return np.array(l)
 
-class board:
+
+class game_board:
     def __init__(self, auto=True, array=np.array([]), size=4):
         if not auto:
             self.board = transform(array)
@@ -95,14 +97,44 @@ class board:
         true_hole = [(i+row, j+col) for i,j in hole]
         for y in range(height):
             for x in range(width):
-                if (row+y, row+x) not in true_hole:
-                    if board[row+y][col+x].piece != 'd':
-                        return False
+                if row+y>=self.size or col+x>=self.size:
+                    return False
+                else:
+                    if (row+y, col+x) not in true_hole:
+                        if self.board[row+y][col+x].piece != ' ':
+                            return False
         return True
 
     def place_block(self, height, width, row, col, piece, hole=[]):
         true_hole = [(i+row, j+col) for i,j in hole]
         for y in range(height):
             for x in range(width):
-                if (row+y, row+x) not in true_hole:
+                if (row+y, col+x) not in true_hole and row+y<self.size and col+x<self.size:
                     self.set_piece(row+y,col+x,piece)
+
+    def randomize(self, piece: block, debug):
+        r = rd.randint(0,self.size)
+        c = rd.randint(0,self.size)
+        o = rd.randint(0,piece.orientation-1)
+        holes = []
+        for old_hole in piece.holes:
+            holes.append(transform_hole(old_hole, o, piece.piece))
+        if self.check_fit(piece.height, piece.width, r, c, holes):
+            self.place_block(piece.height, piece.width, r, c, piece.piece, holes)
+            if debug:
+                print(f"randomize() --> success (r,c,o) = ({r},{c},{o})")
+            return True
+        else:
+            if debug:
+                print(f"randomize() --> Failed (r,c,o) = ({r},{c},{o})")
+            return False
+
+    def randomize_pieces(self, pieces, debug=False):
+        new_list = sorted([[piece.area()-len(piece.holes), piece.piece, piece] for piece in pieces], reverse=True)
+        for area, name, piece in new_list:
+            for i in range(threshold):
+                if self.randomize(piece,debug):
+                    if debug:
+                        print(f"randomize_pieces --> success {piece.piece}")
+                    break  
+                 
