6fa8c0a1a3ca6e675f07247bd2e345bbf87d81b3
GemsLoveNLP <6438169421@student.chula.ac.th>
2023-04-25 01:31:54 +0700

    better video

--- cvtry.py
+++ cvtry.py
@@ -1,37 +1,7 @@
-import pygame
-import cv2
+def get_file_name(dir, i, folder_size):
+    i = i%folder_size + 1
+    string = dir+"0"*(4-len(str(i)))+str(i)+".png"
+    return string
 
-video = cv2.VideoCapture("tutorial.mp4")
-success, video_image = video.read()
-fps = video.get(cv2.CAP_PROP_FPS)
-
-window = pygame.display.set_mode((700,700))
-clock = pygame.time.Clock()
-
-run = success
-i = 0
-while run:
-    clock.tick(30)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    
-    # success, video_image = video.read()
-    # if success:
-    #     video_surf = pygame.image.frombuffer(
-    #         video_image.tobytes(), video_image.shape[1::-1], "BGR")
-    # else:
-    #     run = False
-    # window.blit(video_surf, (0, 0))
-    # pygame.display.flip()
-    
-    img = pygame.image.load(f"C:\\Users\\user\\Desktop\\Python\\BBC\\tutorial\\tutorial ({i%531}).png")
-    # img = pygame.image.load("ring.png")
-    timg = pygame.transform.scale(img,(700,700))
-    window.blit(timg, (0,0))
-    pygame.display.update()
-
-    i+=1
-
-pygame.quit()
-exit()
+for i in range(0,110,10):
+    print(get_file_name("tutorial",i, 90))
